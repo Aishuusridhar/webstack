9caf28cfb871bf60db0eb7ea1f9e2ad3c30e29d1
Aishuusridhar <69647454+Aishuusridhar@users.noreply.github.com>
2020-12-10 23:45:16 +0530

    Update spapj.js

--- spapj.js
+++ spapj.js
@@ -6,7 +6,7 @@ angular.module("myapp", ['ngRoute'])
             }).when('/pjtwo', {
                templateUrl: 'pjtwo.html',
                controller:'onectrl'
-            }).when('/pjtwo/:first/:last', {
+            }).when('/pjtwo', {
                templateUrl: 'pjtwo.html',
                controller:'onectrl'
              }).when('/pjthree', {
